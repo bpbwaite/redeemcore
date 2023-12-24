474df4dc655ee324b4c5d80fd25014a878bcbf76
Briellyn Braithwaite <bpbraithwaite@gmail.com>
2023-12-24 15:38:10 -0800

    hotfix with redefinition

--- RedemptionCore/internal.py
+++ RedemptionCore/internal.py
@@ -1,7 +1,8 @@
 from threading import Thread
 from pathlib import Path
 from functools import partial
-from http.server import SimpleHTTPRequestHandler, BaseHTTPRequestHandler
+from itertools import chain
+from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
 from socket import gethostname
 from typing import Any
@@ -9,11 +10,14 @@ from typing import Any
 from .settings import logger
 
 class loggingServer(SimpleHTTPRequestHandler):
+    cct = str.maketrans(
+            {c: fr'\x{c:02x}' for c in chain(range(0x20), range(0x7f,0xa0))})
+    # redefinition of control table
+    # sometimes python can't find the attribute of a super-super class
     def log_message(self, format: str, *args: Any) -> None:
-        cct = BaseHTTPRequestHandler._control_char_table
         logger.debug('Serving %s : %s' %
                     (self.address_string(),
-                    (format % args).translate(cct)))
+                    (format % args).translate(self.cct)))
 
 def serve(directory: Path, port: int = 3001):
     try:
