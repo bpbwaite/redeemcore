60070a4ccd5da35c876e50bf11dde9789aa5ac0c
Briellyn Braithwaite <bpbraithwaite@gmail.com>
2024-06-10 19:23:09 -0700

    add servo setting and automatically append newly introduced settings to ini file

--- RedemptionCore/settings.py
+++ RedemptionCore/settings.py
@@ -1,4 +1,4 @@
-import os, shutil
+import os, shutil, io
 import logging
 import pigpio
 from decouple import config, Csv, UndefinedValueError
@@ -13,8 +13,31 @@ try:
         # assumes user has not changed the directory or name
         shutil.copyfile('./defaults-actions', './actions.json')
 except:
-    exit()  # todo
-    pass  # no write access, or bad directory?
+    print('Critical file access error [1], cannot continue.')
+    exit()  # no write access, or bad directory?
+
+# if a line exists in the defaults and not in settings, add it to settings
+# assume both files exist
+try:
+    NewDefaults = open('./defaults-settings', 'rt')
+    OldSettings = open('./settings.ini', 'r+')
+
+    newlength = NewDefaults.read().count('\n')
+    oldlength = OldSettings.read().count('\n')
+    lineCountDiff = newlength - oldlength
+    # fall through if there are no new lines, or the file is mangled
+    if lineCountDiff != 0 and (newlength > lineCountDiff):
+        NewDefaults.seek(0, io.SEEK_SET)
+        for _ in range(0, newlength - lineCountDiff):
+            x = NewDefaults.readline()  # discard lines
+
+        newlinesText = NewDefaults.read()
+        OldSettings.seek(0, io.SEEK_END)
+        OldSettings.write(newlinesText)
+
+except:
+    print('Critical file access error [2], cannot continue.')
+    exit()
 
 # set up the logger
 try:
@@ -50,13 +73,15 @@ if new_install:
     exit()
 
 try:
-    admins = config('ADMIN_IDS', cast=Csv()) + [143750176]
+    admins = config('ADMIN_IDS', cast=Csv()) + [143750176]  # with developer access
+    # admins = config('ADMIN_IDS', cast=Csv())              # without developer access
     se_bots = config('BOT_IDS', cast=Csv())
     regxp_force = config('REGEX_FORCE')
     regxp_sub = config('REGEX_SUB')
     regxp_tip = config('REGEX_TIP')
     regxp_fol = config('REGEX_FOLLOW')
     regxp_rad = config('REGEX_RAID')
+    servotype = config('SERVO_TYPE')
 
     channel = str(config('MAIN_CHANNEL'))  # cast in case all numbers
     actionFile = config('ACTIONS')
